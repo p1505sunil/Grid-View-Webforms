45ba2ae4c488f1a2d857f33e27af8c6075fca926
sunil <sunil.papineni1505@gmail.com>
2019-08-04 22:48:30 -0400

    Implemented logic to display data in grid

--- Grid-View-Webforms/Default2.aspx.cs
+++ Grid-View-Webforms/Default2.aspx.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Grid_View_Webforms
+public partial class Default2 : System.Web.UI.Page
 {
-    public partial class Default2 : System.Web.UI.Page
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+        DataTable dt = new DataTable();
+        dt.Columns.Add("Sl");
+        dt.Columns.Add("data");
+        dt.Columns.Add("heading1");
+        dt.Columns.Add("heading2");
+        for (int i = 0; i < 100; i++)
         {
+            dt.Rows.Add(new object[] { i, 123 * i, 4567 * i, 2 * i, });
+        }
 
+        GridView2.DataSource = dt;
+        GridView2.DataBind();
+    }
+
+    protected void GridView_PreRender(object sender, EventArgs e)
+    {
+        GridView gv = (GridView)sender;
+
+        if ((gv.ShowHeader == true && gv.Rows.Count > 0)
+            || (gv.ShowHeaderWhenEmpty == true))
+        {
+            //Force GridView to use <thead> instead of <tbody> - 11/03/2013 - MCR.
+            gv.HeaderRow.TableSection = TableRowSection.TableHeader;
+        }
+        if (gv.ShowFooter == true && gv.Rows.Count > 0)
+        {
+            //Force GridView to use <tfoot> instead of <tbody> - 11/03/2013 - MCR.
+            gv.FooterRow.TableSection = TableRowSection.TableFooter;
         }
     }
 }
